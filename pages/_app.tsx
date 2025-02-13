a508b47646ecd84c795d29a2f5bddb3406de1154
Dom Nguyen <nguyendaithang23061997@gmail.com>
2025-02-13 11:26:14 +0700

    build project

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { inter, red_hat_display } from "@/styles/fonts";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={`${inter.variable} ${red_hat_display.variable}`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  );
+}
