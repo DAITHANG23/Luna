dae2473e531f14d270fef1c03e67dbfed6c78e9e
Dom Nguyen <nguyendaithang23061997@gmail.com>
2025-03-12 00:25:40 +0700

    feat: build footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <div className="text-primary-text">
-      <p>Footer</p>
+    <div className="text-white grid grid-cols-1 sm:grid-cols-3 w-full p-[50px] gap-[50px] bg-[#1C252E]">
+      <div className="w-[150px] lg:w-[280px]">
+        <h4 className="text-white">Domique Fusion Corporation</h4>
+        <p className="text-secondary-text">
+          Address: Thong Nhat Street, Go Vap Distric, Ho Chi Minh City
+        </p>
+        <p className="text-secondary-text">
+          Responsible for content: Mr.Dom Nguyen
+        </p>
+      </div>
+
+      <div>
+        <h4 className="text-white">Cusomer Support</h4>
+        <p className="text-secondary-text">Terms of Use</p>
+        <p className="text-secondary-text">Membership policy</p>
+        <p className="text-secondary-text">Privacy policy</p>
+      </div>
+
+      <div>
+        <h4 className="text-white">{`© ${currentYear} Domique Fushion.,JSC. All rights reserved`}</h4>
+      </div>
     </div>
   );
 };
--- pages/components/ConceptsList.tsx
+++ pages/components/ConceptsList.tsx
@@ -1,315 +1,212 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import clsx from "clsx";
+
+interface Restaurant {
+  name: string;
+  img1: string;
+  img2: string;
+  url: string;
+  width: number;
+  height: number;
+}
+interface RestaurantsConceptType {
+  type: string;
+  items: Array<Restaurant>;
+}
+
+const RESTAURANTS_CONCEPT = [
+  {
+    type: "HOTPOT",
+    items: [
+      {
+        name: "Kichi-Kichi",
+        img1: "/assets/images/kichi-01.png",
+        img2: "/assets/images/kichi-02.png",
+        url: "/kichi-kichi",
+        width: 134,
+        height: 55,
+      },
+      {
+        name: "Hutong",
+        img1: "/assets/images/hutong-01.png",
+        img2: "/assets/images/hutong-02.png",
+        url: "/hutong",
+        width: 134,
+        height: 55,
+      },
+      {
+        name: "Manwah",
+        img1: "/assets/images/manwah-01.png",
+        img2: "/assets/images/manwah-02.png",
+        url: "/manwah",
+        width: 55,
+        height: 55,
+      },
+    ],
+  },
+  {
+    type: "BBQ",
+    items: [
+      {
+        name: "Kpub",
+        img1: "/assets/images/kpub-01.jpg",
+        img2: "/assets/images/kpub-02.jpg",
+        url: "/kpub",
+        width: 55,
+        height: 55,
+      },
+      {
+        name: "Gogi",
+        img1: "/assets/images/gogi2.png",
+        img2: "/assets/images/gogi1.png",
+        url: "/gogi",
+        width: 55,
+        height: 55,
+      },
+      {
+        name: "Sumo Yakiniku",
+        img1: "/assets/images/sumo2.png",
+        img2: "/assets/images/sumo1.png",
+        url: "/sumo-yakiniku",
+        width: 55,
+        height: 55,
+      },
+    ],
+  },
+  {
+    type: "JAPANESE",
+    items: [
+      {
+        name: "ISuShi",
+        img1: "/assets/images/isushi-1.png",
+        img2: "/assets/images/isushi-2.png",
+        url: "/isushi",
+        width: 65,
+        height: 55,
+      },
+      {
+        name: "Daruma",
+        img1: "/assets/images/daruma-1.png",
+        img2: "/assets/images/daruma-2.png",
+        url: "/daruma",
+        width: 55,
+        height: 55,
+      },
+      {
+        name: "Shogun",
+        img1: "/assets/images/shogun-1.png",
+        img2: "/assets/images/shogun-2.png",
+        url: "/shogun",
+        width: 65,
+        height: 55,
+      },
+    ],
+  },
+  {
+    type: "STEAK HOUSE",
+    items: [
+      {
+        name: "Woomaster",
+        img1: "/assets/images/woomaster2.png",
+        img2: "/assets/images/woomaster1.png",
+        url: "/woomaster",
+        width: 72,
+        height: 55,
+      },
+    ],
+  },
+  {
+    type: "OTHER",
+    items: [
+      {
+        name: "Phongon37",
+        img1: "/assets/images/phongon_37_1.png",
+        img2: "/assets/images/phongon_37_2.png",
+        url: "/phongon37",
+        width: 55,
+        height: 55,
+      },
+      {
+        name: "Cloudpot",
+        img1: "/assets/images/cloudpot-2.png",
+        img2: "/assets/images/cloudpot-1.png",
+        url: "/cloudpot",
+        width: 80,
+        height: 55,
+      },
+      {
+        name: "Crystal Jade",
+        img1: "/assets/images/crystal-1.png",
+        img2: "/assets/images/crystal-2.png",
+        url: "/crystal-jade",
+        width: 80,
+        height: 55,
+      },
+      {
+        name: "Hang cuon",
+        img1: "/assets/images/hangcuon2.png",
+        img2: "/assets/images/hangcuon1.png",
+        url: "/hangcuon",
+        width: 200,
+        height: 55,
+      },
+    ],
+  },
+] as Array<RestaurantsConceptType>;
 
 const ConceptsList = () => {
   return (
     <div
-      className="not-prose sm:w-[80%] lg:w-[60%] pt-[100px] pb-[50px] m-auto"
+      className="not-prose w-full p-4 sm:w-[80%] lg:w-[60%] pt-[100px] pb-[50px] m-auto"
       style={{ fontFamily: "Inter" }}
     >
-      <div className="flex">
-        <div className="border-r border-dashed border-gray-300 sm:min-w-[150px] h-[60px] ">
-          <h3 className="text-[18px] font-bold">HOTPOT</h3>
-          <p className="text-xs">CONCEPT</p>
-        </div>
-        <ul className="grid grid-cols-3 gap-4 w-[50%] h-auto px-4 border-b border-dashed border-gray-300">
-          <li className="relative">
-            <Link href="/kichi-kichi">
-              <Image
-                src="/assets/images/kichi-01.png"
-                alt="default"
-                width={128}
-                height={55}
-                className="absolute inset-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/kichi-02.png"
-                alt="hover"
-                width={128}
-                height={55}
-                className="absolute inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-
-          <li className="relative">
-            <Link href="/hutong">
-              <Image
-                src="/assets/images/hutong-01.png"
-                alt="default"
-                width={128}
-                height={55}
-                className="absolute top-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/hutong-02.png"
-                alt="hover"
-                width={128}
-                height={128}
-                className="absolute inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-
-          <li className="relative">
-            <Link href="/manwah">
-              <Image
-                src="/assets/images/manwah-01.png"
-                alt="default"
-                width={128}
-                height={128}
-                className="absolute w-[55px] h-[55px] top-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/manwah-02.png"
-                alt="hover"
-                width={128}
-                height={128}
-                className="absolute w-[55px] h-[55px] objective-fit inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="flex pt-[20px]">
-        <div className="border-r border-dashed border-gray-300 sm:min-w-[150px] h-[60px]">
-          <h3 className="text-[18px] font-bold">BBQ</h3>
-          <p className="text-xs">CONCEPT</p>
-        </div>
-        <ul className="grid grid-cols-3 gap-4 w-[50%] h-auto px-4 border-b border-dashed border-gray-300">
-          <li className="relative">
-            <Link href="/kpub">
-              <Image
-                src="/assets/images/kpub-01.jpg"
-                alt="default"
-                width={128}
-                height={55}
-                className="absolute w-[55px] h-[55px] inset-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/kpub-02.jpg"
-                alt="hover"
-                width={128}
-                height={55}
-                className="absolute w-[55px] h-[55px] inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-
-          <li className="relative">
-            <Link href="/gogi">
-              <Image
-                src="/assets/images/gogi2.png"
-                alt="default"
-                width={128}
-                height={55}
-                className="absolute w-[55px] h-[55px] top-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/gogi1.png"
-                alt="hover"
-                width={128}
-                height={128}
-                className="absolute w-[55px] h-[55px] inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-
-          <li className="relative">
-            <Link href="/sumo-yakiniku">
-              <Image
-                src="/assets/images/sumo2.png"
-                alt="default"
-                width={128}
-                height={128}
-                className="absolute w-[55px] h-[55px] top-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/sumo1.png"
-                alt="hover"
-                width={128}
-                height={128}
-                className="absolute w-[55px] h-[55px] objective-fit inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-        </ul>
-      </div>
-
-      <div className="flex pt-[20px]">
-        <div className="border-r border-dashed border-gray-300 sm:min-w-[150px] h-[60px]">
-          <h3 className="text-[18px] font-bold">JAPANESE</h3>
-          <p className="text-xs">CONCEPT</p>
-        </div>
-        <ul className="grid grid-cols-3 gap-4 w-[50%] h-auto px-4 border-b border-dashed border-gray-300">
-          <li className="relative">
-            <Link href="/isushi">
-              <Image
-                src="/assets/images/isushi-1.png"
-                alt="default"
-                width={128}
-                height={55}
-                className="absolute w-[65px] h-[55px] inset-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/isushi-2.png"
-                alt="hover"
-                width={128}
-                height={55}
-                className="absolute w-[65px] h-[55px] inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-
-          <li className="relative">
-            <Link href="/daruma">
-              <Image
-                src="/assets/images/daruma-1.png"
-                alt="default"
-                width={128}
-                height={55}
-                className="absolute w-[55px] h-[55px] top-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/daruma-2.png"
-                alt="hover"
-                width={128}
-                height={128}
-                className="absolute w-[55px] h-[55px] inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-
-          <li className="relative">
-            <Link href="/shogun">
-              <Image
-                src="/assets/images/shogun-1.png"
-                alt="default"
-                width={128}
-                height={128}
-                className="absolute w-[65px] h-[55px] top-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/shogun-2.png"
-                alt="hover"
-                width={128}
-                height={128}
-                className="absolute w-[65px] h-[55px] objective-fit inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-        </ul>
-      </div>
-
-      <div className="flex pt-[20px]">
-        <div className="border-r border-dashed border-gray-300 sm:min-w-[150px] h-[60px]">
-          <h3 className="text-[18px] font-bold">STEAK HOUSE</h3>
-          <p className="text-xs">CONCEPT</p>
-        </div>
-        <ul className="grid grid-cols-3 gap-4 min-w-[250px] h-auto px-4 border-b border-dashed border-gray-300">
-          <li className="relative">
-            <Link href="/woomaster">
-              <Image
-                src="/assets/images/woomaster2.png"
-                alt="default"
-                width={55}
-                height={55}
-                className="absolute w-[72px] h-[55px] inset-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/woomaster1.png"
-                alt="hover"
-                width={128}
-                height={55}
-                className="absolute w-[72px] h-[55px] inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-        </ul>
-      </div>
-
-      <div className="flex pt-[20px]">
-        <div className="border-r border-dashed border-gray-300 sm:min-w-[150px] h-[60px]">
-          <h3 className="text-[18px] font-bold">OTHER</h3>
-          <p className="text-xs">CONCEPT</p>
-        </div>
-        <ul className="grid grid-cols-4 gap-4 w-[60%] h-auto px-4 border-b border-dashed border-gray-300">
-          <li className="relative">
-            <Link href="/phongon37">
-              <Image
-                src="/assets/images/phongon_37_1.png"
-                alt="default"
-                width={55}
-                height={55}
-                className="absolute w-[55px] h-[55px] inset-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/phongon_37_2.png"
-                alt="hover"
-                width={128}
-                height={55}
-                className="absolute w-[55px] h-[55px] inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-          <li className="relative">
-            <Link href="/cloudpot">
-              <Image
-                src="/assets/images/cloudpot-2.png"
-                alt="default"
-                width={55}
-                height={55}
-                className="absolute w-[80px] h-[55px] inset-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/cloudpot-1.png"
-                alt="hover"
-                width={128}
-                height={55}
-                className="absolute w-[80px] h-[55px] inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-          <li className="relative">
-            <Link href="/crystal-jade">
-              <Image
-                src="/assets/images/crystal-1.png"
-                alt="default"
-                width={55}
-                height={55}
-                className="absolute w-[80px] h-[55px] inset-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/crystal-2.png"
-                alt="hover"
-                width={128}
-                height={55}
-                className="absolute w-[80px] h-[55px] inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-
-          <li className="relative">
-            <Link href="/hangcuon">
-              <Image
-                src="/assets/images/hangcuon2.png"
-                alt="default"
-                width={155}
-                height={55}
-                className="absolute w-[145px] h-[55px] inset-0 transition-opacity duration-300 hover:opacity-0"
-              />
-              <Image
-                src="/assets/images/hangcuon1.png"
-                alt="hover"
-                width={128}
-                height={55}
-                className="absolute w-[145px] h-[55px] inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
-              />
-            </Link>
-          </li>
-        </ul>
-      </div>
+      {RESTAURANTS_CONCEPT.map((i) => {
+        return (
+          <div key={i.type} className="flex sm:flex-row flex-col pt-[20px]">
+            <div className="sm:border-r border-b sm:border-b-0 border-dashed border-gray-300 sm:min-w-[150px] sm:h-[60px] flex flex-row items-center gap-2 sm:flex-col sm:items-start sm:gap-0">
+              <h3 className="text-[18px] font-bold dark:text-secondary-text">
+                {i.type}
+              </h3>
+              <p className="sm:text-xs text-[18px] font-bold sm:font-normal dark:text-secondary-text">
+                CONCEPT
+              </p>
+            </div>
+            <ul
+              className={clsx(
+                i.type === "OTHER"
+                  ? "sm:grid-cols-4 2xl:w-[70%] sm:w-[80%]"
+                  : "sm:grid-cols-3 2xl:w-[60%] sm:w-[90%]",
+                "grid grid-cols-2 gap-4 w-full sm:h-auto h-[150px] p-1 px-4 sm:border-b sm:border-dashed sm:border-gray-300"
+              )}
+            >
+              {i.items.map((restaurant) => {
+                return (
+                  <li key={restaurant.name} className="relative">
+                    <Link href={restaurant.url}>
+                      <Image
+                        src={restaurant.img1}
+                        alt={restaurant.name}
+                        width={restaurant.width}
+                        height={restaurant.height}
+                        className="absolute h-[55px] inset-0 transition-opacity duration-300 hover:opacity-0"
+                      />
+                      <Image
+                        src={restaurant.img2}
+                        alt={restaurant.name}
+                        width={restaurant.width}
+                        height={restaurant.height}
+                        className="absolute h-[55px] inset-0 opacity-0 transition-opacity duration-300 hover:opacity-100"
+                      />
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
