8885e1fe75e54e469c8838534009a9ca1ac749be
Dom Nguyễn <57473628+DAITHANG23@users.noreply.github.com>
2025-04-19 16:59:23 +0700

    fix: fix bug UI

--- share/components/SliderComponent.tsx
+++ share/components/SliderComponent.tsx
@@ -29,7 +29,7 @@ const SliderComponent = ({
       <Slider
         {...settings}
         className={clsx(
-          isSmallSize ? "w-[90%] sm:!w-[60%] " : "w-full",
+          isSmallSize ? "w-[90%] xl:!w-[80%] " : "w-full",
           " text-center items-center m-auto"
         )}
       >
@@ -39,16 +39,15 @@ const SliderComponent = ({
               key={i.name}
               className={clsx(
                 isSmallSize
-                  ? "h-[219px] sm:h-[300px] xl:h-[500px] border-2 border-primary/20 shadow-lg hover:shadow-xl transition-all duration-300 rounded-md mb-10"
-                  : "h-[219px] xl:h-[1000px] sm:h-[500px]",
+                  ? "h-[300px] sm:h-[300px] lg:h-[500px] border-2 border-primary/20 shadow-lg hover:shadow-xl transition-all duration-300 rounded-md mb-10"
+                  : "h-[219px] xl:h-[1000px] sm:h-[600px]",
                 "w-full flex items-center justify-center overflow-hidden relative"
               )}
             >
               <Image
                 src={i.img}
                 alt={i.name}
-                width={10000}
-                height={500}
+                fill
                 className="object-cover sm:object-fill hover:scale-105 transition-transform duration-500"
               />
             </div>
