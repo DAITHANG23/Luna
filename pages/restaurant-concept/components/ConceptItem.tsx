81d23335cee56fee3a11036e14d217ef6d6b7e0e
Dom Nguyễn <57473628+DAITHANG23@users.noreply.github.com>
2025-04-22 23:10:56 +0700

    feat: build concepts

--- pages/restaurant-concept/components/ConceptItem.tsx
+++ pages/restaurant-concept/components/ConceptItem.tsx
@@ -1,13 +1,26 @@
 import { ConceptModel } from "@/@types/models/concept";
 import Image from "next/image";
-import React from "react";
-
+import React, { useState } from "react";
+import { HeartIcon } from "@heroicons/react/24/outline";
+import { HeartIcon as HeartIconSolid } from "@heroicons/react/24/solid";
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
+import { CheckCircleIcon as CheckCircleIconSolid } from "@heroicons/react/24/solid";
+import { Square2StackIcon } from "@heroicons/react/24/solid";
+import ModalCarousel from "@/share/components/ModalCarousel";
 interface ConceptItemProps {
   concept: ConceptModel;
 }
 const ConceptItem = ({ concept }: ConceptItemProps) => {
+  const [isCheckedInConcept, setIsCheckedInConcept] = useState(false);
+  const [isFavoriteConcept, setIsFavoriteConcept] = useState(false);
+  const [isOpenModalImageList, setIsOpenModalImageList] = useState(false);
   return (
-    <div className="h-[450px] flex flex-col border-2 border-gray-300 rounded-lg shadow-md cursor-pointer hover:shadow-xl duration-300 transition-all ease-in-out dark:shadow-md dark:hover:shadow-[0_8px_20px_rgba(255,255,255,0.15)] dark:transition-shadow dark:duration-300">
+    <div className="relative h-[450px] flex flex-col border-2 border-gray-300 rounded-lg shadow-md cursor-pointer hover:shadow-xl duration-300 transition-all ease-in-out dark:shadow-md dark:hover:shadow-[0_8px_20px_rgba(255,255,255,0.15)] dark:transition-shadow dark:duration-300">
+      <ModalCarousel
+        setOpen={setIsOpenModalImageList}
+        open={isOpenModalImageList}
+        imagesList={concept.images}
+      />
       <div className="w-full h-[300px] relative ">
         <Image
           src={`/assets/images/${concept?.imageCover}.jpg`}
@@ -15,12 +28,46 @@ const ConceptItem = ({ concept }: ConceptItemProps) => {
           fill
           className="rounded-tl-lg rounded-tr-lg rounded-bl-none rounded-br-none"
         />
+        <button
+          className="absolute bottom-4 right-4 text-white w-6 h-6"
+          onClick={() => setIsOpenModalImageList(true)}
+        >
+          <Square2StackIcon className="text-white w-8 h-8" />
+        </button>
       </div>
       <div className="p-4 flex flex-col justify-start items-start gap-2">
         <h3 className="text-primary-text">{concept?.name}</h3>
         <p className="text-primary-text">{concept?.type}</p>
         <p className="text-primary-text">{`${concept?.address}`}</p>
         <p className="text-primary">{`${concept?.totalRatings}`}</p>
+      </div>
+      <div className="absolute top-[10px] right-[10px] flex gap-3">
+        <button
+          className="border-none bg-white rounded-full p-1 cursor-pointer"
+          onClick={() => {
+            setIsCheckedInConcept((prev) => !prev);
+          }}
+        >
+          {isCheckedInConcept ? (
+            <CheckCircleIconSolid className="text-green-500 w-5 h-5" />
+          ) : (
+            <CheckCircleIcon className="text-black w-5 h-5" />
+          )}
+        </button>
+        <button
+          className="border-none bg-white rounded-full p-1 cursor-pointer"
+          onClick={() => {
+            setIsFavoriteConcept((prev) => !prev);
+          }}
+        >
+          <div className="w-5 h-5">
+            {isFavoriteConcept ? (
+              <HeartIconSolid className="text-primary w-full h-full" />
+            ) : (
+              <HeartIcon className="text-black w-full h-full" />
+            )}
+          </div>
+        </button>
       </div>
     </div>
   );
---
+++ share/components/WrapperFilter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WrapperFilter = () => {
+  return <div>WrapperFilter</div>;
+};
+
+export default WrapperFilter;
