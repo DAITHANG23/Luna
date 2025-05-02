40ed02d9e3ab1c482320f7b7ce77a27ff86f1393
Dom Nguyễn <57473628+DAITHANG23@users.noreply.github.com>
2025-05-02 09:43:34 +0700

    fix: Fix UI

--- pages/favorites-concepts/components/FavoriteRestaurantsView.tsx
+++ pages/favorites-concepts/components/FavoriteRestaurantsView.tsx
@@ -2,20 +2,21 @@ import Image from "next/image";
 import React from "react";
 import { HeartIcon as HeartIconSolid } from "@heroicons/react/24/solid";
 import { ConceptModel } from "@/@types/models/concept";
+import { useTranslation } from "react-i18next";
 
 interface FavoriteConceptsViewProps {
   data: Array<ConceptModel>;
 }
 const FavoriteConceptsView = ({ data }: FavoriteConceptsViewProps) => {
   const favoriteImages = data?.map((i) => i.imageCover);
-
+  const { t } = useTranslation("concept");
   return (
-    <div className="flex flex-col w-[21.75rem] !h-[19.7rem] shadow-glass border-none rounded-xl cursor-pointer transition duration-300 ease-in-out hover:scale-105">
+    <div className="flex flex-col w-full sm:w-[21.75rem] !h-[19.7rem] shadow-glass border-none rounded-xl cursor-pointer transition duration-300 ease-in-out hover:scale-105">
       <div className="flex text-center  w-full h-[3.3rem] p-4 gap-4 bg-white rounded-xl">
         <p>
-          <HeartIconSolid className="text-primary w-full h-full" />
+          <HeartIconSolid className="text-primary w-5 h-5" />
         </p>
-        <p className="font-base font-normal">Restaurants I Love</p>
+        <p className="font-base font-normal">{t("titleRestaurantFavorite")}</p>
       </div>
       <div className="w-full h-[17rem] relative rounded-xl">
         {favoriteImages && favoriteImages.length >= 3 ? (
