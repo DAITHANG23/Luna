48d1fad570271819fccadb8ebe78421a031ed1d7
Dom Nguyễn <57473628+DAITHANG23@users.noreply.github.com>
2025-07-02 14:50:54 +0700

    feat(enhance): enhance logic notifications

--- pages/notifications/components/NotificationMain.tsx
+++ pages/notifications/components/NotificationMain.tsx
@@ -1,5 +1,6 @@
 import { NotificationModel } from "@/@types/models";
 import apiService from "@/api";
+import useCheckReadNotification from "@/features/hooks/NotificationBooking/useCheckReadNotification";
 import { useAppDispatch, useAppSelector } from "@/libs/redux/hooks";
 import { getAllNotifications } from "@/libs/redux/masterDataSlice";
 import { WrapperFilter } from "@/libs/shared/components";
@@ -19,6 +20,7 @@ const NotificationMain = ({ children }: NotificationMainProps) => {
   const allNotifications = useAppSelector(
     (state) => state.masterData.allNotifications
   )?.data.data;
+
   const unReadNotificationsQuantities = useAppSelector(
     (state) => state.masterData.unReadNotificationsQuantity
   );
@@ -30,17 +32,43 @@ const NotificationMain = ({ children }: NotificationMainProps) => {
     setSelectedId(id);
   };
   const dispatch = useAppDispatch();
+
+  const { mutate: checkReadNotification } = useCheckReadNotification();
+
+  const handleAfterDeleteNotification = useCallback(
+    (id: string) => {
+      const index = allNotifications?.findIndex(
+        (notification: NotificationModel) => notification._id === id
+      );
+      const newList = allNotifications?.filter((n) => n._id !== id);
+
+      if (selectedId === id) {
+        const nextIdNotifcation = newList?.[index!]?._id;
+        setSelectedId(nextIdNotifcation || null);
+
+        router.push(
+          nextIdNotifcation
+            ? `/notifications/${nextIdNotifcation}`
+            : "/notifications"
+        );
+        checkReadNotification(nextIdNotifcation || "");
+      }
+    },
+    [allNotifications, selectedId, router, checkReadNotification]
+  );
+
   const handleDeleteNotification = useCallback(
     async (id: string) => {
       try {
         await apiService.notifications.deleteNotification({ id });
         dispatch(getAllNotifications());
-        router.push("/notifications");
+
+        handleAfterDeleteNotification(id);
       } catch (error) {
         console.error("Failed to delete notification:", error);
       }
     },
-    [dispatch, router]
+    [dispatch, handleAfterDeleteNotification]
   );
 
   const notificationsNavbar = useMemo(() => {
@@ -77,7 +105,7 @@ const NotificationMain = ({ children }: NotificationMainProps) => {
   ]);
   if (!ready) return null;
   return (
-    <div className="mt-[5rem] sm:mt-[7.5rem] w-[90%] xl:w-[70%] h-auto lg:h-[40rem] mx-auto bg-white dark:bg-gray-900 rounded-lg my-4 shadow-lg p-4 lg:py-4 lg:pr-4">
+    <div className="mt-[5rem] sm:mt-[7.5rem] w-[90%] 2xl:w-[70%] h-auto lg:h-[40rem] mx-auto bg-white dark:bg-gray-900 rounded-lg my-4 shadow-lg p-4 lg:py-4 lg:pr-4">
       <div className="flex lg:flex-row flex-col gap-4 h-full">
         <div className="w-full lg:w-[30%] h-full overflow-auto hidden lg:block">
           {notificationsNavbar}
