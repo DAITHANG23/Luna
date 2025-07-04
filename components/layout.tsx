c4427f14df3b1886e8286ad442b9e2a54463816d
Dom Nguyễn <57473628+DAITHANG23@users.noreply.github.com>
2025-07-04 17:15:03 +0700

    feat(update): use middleware

--- components/layout.tsx
+++ components/layout.tsx
@@ -13,6 +13,7 @@ import AuthInitializer from "./AuthInitializer";
 import { isEmpty } from "lodash";
 import { ROUTERS } from "@/contants";
 import { getAllNotifications } from "@/libs/redux/masterDataSlice";
+import cookie from "@/utils/cookies";
 
 export default function Layout({
   children,
@@ -25,30 +26,16 @@ export default function Layout({
   const accountInfo = useAppSelector((state) => state.auth.accountInfo);
 
   useEffect(() => {
-    const initAuth = async () => {
-      try {
-        const res = await fetch("/api/token", {
-          method: "GET",
-          credentials: "include",
-        });
+    const accessTokenCookie = cookie.getAccessToken();
+    const refreshTokenCookie = cookie.getRefreshToken();
 
-        const data = await res.json();
-
-        if (data?.accessToken && data?.refreshToken) {
-          localStorage.setItem("accessToken", data.accessToken);
-          localStorage.setItem("refreshToken", data.refreshToken);
-          dispatch(accessToken({ accessToken: data.accessToken }));
-          dispatch(getAllNotifications());
-        } else {
-          dispatch(logout());
-        }
-      } catch (error) {
-        console.error("Error fetching token:", error);
-        dispatch(logout());
-      }
-    };
-
-    initAuth();
+    if (!accessToken && !refreshTokenCookie) {
+      dispatch(logout());
+    }
+    localStorage.setItem("accessToken", accessTokenCookie);
+    localStorage.setItem("refreshToken", refreshTokenCookie);
+    dispatch(accessToken({ accessToken: accessTokenCookie as string }));
+    dispatch(getAllNotifications());
   }, [dispatch]);
 
   const isLoginPage =
